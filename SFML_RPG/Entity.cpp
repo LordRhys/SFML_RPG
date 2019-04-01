7785a3936d50ffe50dcbe52685786e1a0f4b7c72
LordRhys <lordrhystower@gmail.com>
2019-04-01 18:28:09 -0500

    After Episode 22, Cleaning up to start with Components

--- SFML_RPG/Entity.cpp
+++ SFML_RPG/Entity.cpp
@@ -46,8 +46,7 @@ void Entity::move(const float& dt, const float dir_x, const float dir_y)
 
 void Entity::update(const float & dt)
 {
-  
-  
+    
 }
 
 void Entity::render(sf::RenderTarget* target)
